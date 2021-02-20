5047f1b7ccd04340f7b88226baa54452f8392e6c
enm10k <enm10k@gmail.com>
2021-02-20 16:20:19 +0900

    wip

--- patches/android-simulcast/SimulcastVideoEncoderFactory.java
+++ patches/android-simulcast/SimulcastVideoEncoderFactory.java
@@ -27,13 +27,11 @@ public class SimulcastVideoEncoderFactory implements VideoEncoderFactory {
 
     @Nullable
     @Override
-    @CalledByNative
     public VideoEncoder createEncoder(VideoCodecInfo info) {
         return new SimulcastVideoEncoder(primary, fallback, info);
     }
 
     @Override
-    @CalledByNative
     public VideoCodecInfo[] getSupportedCodecs() {
         return supportedCodecs();
     }
@@ -41,7 +39,7 @@ public class SimulcastVideoEncoderFactory implements VideoEncoderFactory {
     static VideoCodecInfo[] supportedCodecs() {
         List<VideoCodecInfo> codecs = new ArrayList<VideoCodecInfo>();
         codecs.add(new VideoCodecInfo("VP8", new HashMap<>()));
-        codecs.add(new VideoCodecInfo("H264", new HashMap<>()));
+	// TODO: H.264
         return codecs.toArray(new VideoCodecInfo[codecs.size()]);
     }
 
